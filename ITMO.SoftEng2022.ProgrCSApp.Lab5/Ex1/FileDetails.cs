7459155750f832630b3c899211ec34e3e110b6ce
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-05-26 09:43:31 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.ProgrCSApp.Lab5/Ex1/FileDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.SoftEng2022.ProgrCSApp.Lab5Ex1_1 //Передать в метод Main в качестве параметра имя текстового файла  
+{
+    class FileDetails
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.ProgrCSApp.Lab5/Ex2/FileDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.SoftEng2022.ProgrCSApp.Lab5Ex1_2 //Считать содержимое текстового файла в массив  
+{
+    class FileDetails
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            foreach (char ch in contents)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
