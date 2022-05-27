ad3e33d7d85cc4ccde85bbc68aba8d1aa9408540
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-05-27 08:48:57 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.ProgrCSApp.Lab6/Ex2_1/CreateAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.SoftEng2022.ProgrCSApp.Lab6Ex2_1 //Сделайте номер каждого банковского счета уникальным
+{
+    public enum AccountType { Checking, Deposit }
+    class BankAccount
+    {
+        public void Populate(long number, decimal balance)
+        {
+            accNo = number;
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+
+        private static long nextAccNo = 123; //new
+
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+            public string Type()
+        {
+            return accType.ToString();
+        }
+        public static long NextNumber() //new
+        {
+            return nextAccNo++; //new
+        }
+
+    }
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            //Console.Write("Enter the account number   : ");
+            //long number = long.Parse(Console.ReadLine()); //по инструкции
+            
+            long number = BankAccount.NextNumber(); //new
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            created.Populate(number, balance);
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        { 
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.ProgrCSApp.Lab6/Ex2_2/CreateAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.SoftEng2022.ProgrCSApp.Lab6Ex2_2 //Продолжите инкапсуляцию данных класса BankAccount
+{
+    public enum AccountType { Checking, Deposit }
+    class BankAccount
+    {
+       // public void Populate(long number, decimal balance) //по инструкции
+            public void Populate(decimal balance) //new
+        {
+            //accNo = number; // по инструкции
+            accNo = NextNumber(); //new
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+
+        private static long nextAccNo = 123;
+
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+            public string Type()
+        {
+            return accType.ToString();
+        }
+        //public static long NextNumber() //по инструкции
+            private static long NextNumber() //new
+        {
+            return nextAccNo++;
+        }
+
+    }
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            //long number = BankAccount.NextNumber(); // по инструкции
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            //created.Populate(number, balance); // по инструкции
+            created.Populate(balance); //new
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        { 
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+    }
+}
